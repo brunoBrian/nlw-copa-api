4915fc10c3f6d3371551c5a7d2e7a172e9601fa3
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-03 16:08:03 -0300

    adding create pool endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import cors from "@fastify/cors"
 import { PrismaClient } from "@prisma/client"
 import Fastify from "fastify"
+import z from "zod"
+import ShortUniqueId from "short-unique-id"
 
 const prisma = new PrismaClient({
   log: ['query'] // Log de todas as queries no termial
@@ -19,8 +21,32 @@ async function bootstrap() {
     return prisma.pool.count()
   })
 
-   fastify.get('/pools', () => {
+  fastify.get('/pools', () => {
     return prisma.pool.findMany()
+  })
+
+
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string()
+    })
+
+    const { title } = createPoolBody.parse(request.body);
+    const generateCode = new ShortUniqueId({ length: 6 })
+    const code = String(generateCode()).toUpperCase()
+
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code
+      }
+    })
+
+    return reply.status(201).send({
+      code
+    })
   })
 
   await fastify.listen({ port: 3333, host: '0.0.0.0' })
