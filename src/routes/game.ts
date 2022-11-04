4e848c2899cbf0402a31330b7fec4c9d0f4e7a9a
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2022-11-04 18:10:12 -0300

    create endpoint of get pool games

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -1,11 +1,43 @@
 import { FastifyInstance } from "fastify";
+import z from "zod"
 
+import { authenticate } from "../lib/plugins/authenticate";
 import { prisma } from "../lib/prisma";
 
 export async function GameRoutes(fastify: FastifyInstance) {
-  fastify.get('/games/count', async () => {
-    const count = await prisma.user.count()
+  fastify.get('/pools/:id/games', {
+    onRequest: [authenticate]
+  }, async (request) => {
+    const getGameParams = z.object({
+      id: z.string()
+    })
 
-    return { count }
+    const { id } = getGameParams.parse(request.params);
+
+    const games = await prisma.game.findMany({
+      orderBy: {
+        date: 'desc'
+      },
+      include: {
+        guesses: {
+          where: {
+            participant: {
+              userId: request.user.sub,
+              poolId: id
+            }
+          }
+        }
+      }
+    })
+
+    return {
+      games: games.map(game => {
+        return {
+          ...game,
+          guess: game.guesses.length > 0 ? game.guesses[0] : null,
+          guesses: undefined
+        }
+      })
+    }
   })
 }
